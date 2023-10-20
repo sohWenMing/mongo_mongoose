c5eb1ca8f02e9d40bc132094485d6ecb2ee50852
Soh Wen Ming <wenming.soh@gmail.com>
2023-10-20 14:45:30 +0800

    working on schema and model

--- myApp.js
+++ myApp.js
@@ -2,7 +2,20 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: {
+    type: [String],
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
