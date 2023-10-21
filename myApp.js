5b7539088eb878db8cd93c2a312516f1614666c6
Soh Wen Ming <wenming.soh@gmail.com>
2023-10-21 12:00:37 +0800

    find by id

--- myApp.js
+++ myApp.js
@@ -76,7 +76,13 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findEditThenSave = (personId, done) => {
